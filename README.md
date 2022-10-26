0682f2a0cf42ffca581f5580b7ae27cf7cad2f39
LukeCallahan <lukemcal@gmail.com>
2022-10-26 10:43:50 -0700

    add removeVowel to replace a with -
    Co-authored-by: Ruben Giosa <rubenalexgiosa@gmail.com>
    Co-authored-by: Henry Sullivan <sullivanbhenry@gmail.com>

--- README.md
+++ README.md
@@ -1,13 +1,26 @@
-Describe: firstFunction()
+Describe: individualVowel()
 
-  Test: "It should return individual vowels to be used for removal from the string"
-  Code:
-  const vowelArray = ["a","e","i","o","u"];
-  individualVowel(vowelArray);
-  Expected Output: individualVowel = "a", etc. 
+Test: "It should return individual vowels to be used for removal from the string"
+Code:
+const vowelArray = ["a","e","i","o","u"];
+individualVowel(vowelArray);
+Expected Output: individualVowel = "a", "e", "i", "o", "u", etc. 
 
-  Test: "It should describe the second piece of logic for a function."
-  Code:
-  const example2 = ["data"];
-  firstFunction(example2);
-  Expected Output: "Expected outcome for second piece of logic"
+  
+ 
+Describe: removeVowel()
+
+Test: "It should turn each letter of the string into an element in an array."
+Code:
+const text = "random words";
+const word = "a";
+removeVowel(word, text);
+Expected Output: textArray = ["r","a","n","d","o","m","w","o","r","d","s"]
+
+Test: "It should remove the letter "a" from string"
+Code: 
+const vowel = "a";
+const string = "random words";
+removeFirstVowel("a", "random words");
+Expected Output: "r_ndom words"
+
--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,38 @@
+// Business Logic
+
+function individualVowel(vowelArray) {
+  for (let i = 0; i < vowelArray.length; i+=1) {
+    let individualVowelOutput = vowelArray[i];
+    console.log(individualVowelOutput);
+  }
+}
+
+function removeVowel(word, text) {
+  let noVowelsArray = [];
+  
+  textArray = text.split(" ");
+  textArray.forEach(function(element) {
+    if (element.includes("a")) {
+      noVowelElement = element.replaceAll("a", "-");
+      noVowelsArray.push(noVowelElement);
+    } else {
+      noVowelElement = element;
+      noVowelsArray.push(noVowelElement);
+    };
+  })
+    return noVowelsArray;
+}
+
+
+
+
+
+
+
+
+
+// UI Logic
+
 const input = "Believe you can and you're halfway there. Theodore Roosevelt";
 const vowelArray = ["a","e","i","o","u"];
 
@@ -5,21 +40,5 @@ const vowelArray = ["a","e","i","o","u"];
 window.addEventListener("load", function() {
 
 });
-function individualVowel(vowelArray) {
-  for (let i = 0; i < vowelArray.length; i+=1) {
-    let individualVowelOutput = vowelArray[i];
-    console.log(individualVowelOutput);
-  }
-  // vowelArray.forEach(function(vowel) {
-  //   let individualVowelOutput = vowel;
-  //   return individualVowelOutput;
-  //   console.log(individualVowelOutput);
-  // });
-}
 
-individualVowel(vowelArray);
 
-for (let i = 0; i < vowelArray.length; i+=1) {
-  let individualVowelOutput = vowelArray[i];
-  console.log(individualVowelOutput);
-}
